8baf033e63b3665d84d58dc69f184f406c9eb54f
rkrull1956 <roger.krull@yahoo.com>
2024-01-23 09:12:54 -0500

    update with new image

--- src/main/java/com/dmancloud/dinesh/demoapp/DemoAppController.java
+++ src/main/java/com/dmancloud/dinesh/demoapp/DemoAppController.java
@@ -1,5 +1,10 @@
 package com.dmancloud.dinesh.demoapp;
 
+import java.io.File;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +15,20 @@ public class DemoAppController {
     @GetMapping("/")
     public String hello(Model model) {
         model.addAttribute("message", "Hello World! This is a sample application to demonstrate an end-2-end DevOps Pipeline");
+ 
         return "index";
     }
 
     @GetMapping("/error")
     public String handleError() {
         return "error";
+    }
+
+    public Set<String> listFilesUsingJavaIO(String dir) {
+        return Stream.of(new File(dir).listFiles())
+        .filter(file -> !file.isDirectory())
+        .map(File::getName)
+        .collect(Collectors.toSet());
     }
 }
 
